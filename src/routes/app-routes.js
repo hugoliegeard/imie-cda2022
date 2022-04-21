2f604d9ac0d102032cb193e25abcb842d5cefef0
Hugo LIEGEARD <hugo@hl-media.co>
2022-04-21 11:28:28 +0200

    update

--- src/routes/app-routes.js
+++ src/routes/app-routes.js
@@ -10,7 +10,7 @@ const contactController = require('../controllers/contact-controller');
 // -- Chargement des routes
 router.get('/', defaultController.index);
 router.get('/contacts', defaultController.contacts);
-router.get('/contact', defaultController.contact);
+router.get('/contact/:id', defaultController.contact);
 
 // -- Ajouter, Editer, Supprimer un Contact
 router.get('/ajouter-un-contact', contactController.create_get);
